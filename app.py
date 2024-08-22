38abada0f140d49a5a9c861d37737a57ffb7ea9e
Paula-Stefany <paula.sfa@outlook.com>
2024-08-22 18:13:00 -0300

    calculadora part2 (final)

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
-@app.route('/Calculadora')
+@app.route('/Calculadora', methods=['GET', 'POST'])
 def calculadora():
+
+    if request.method == 'POST':
+        value = float(request.form['initial-value'])
+        fees = float(request.form['compound-interest'])
+        months = int(request.form['months'])
+
+        result = value * ((1 + (fees/100)) ** months)
+        result = round(result, 2)
+
+        return render_template('calculadora.html', result=result)
+
     return render_template('calculadora.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
